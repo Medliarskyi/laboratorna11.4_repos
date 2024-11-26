950a440d97dfadb111883c3b55a68dbf042ed41a
Medliarskyi <med.uras111@gmail.com>
2024-11-26 23:20:11 +0200

    first master commit

---
+++ UnitTest11.4/UnitTest11.4.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <fstream>
+#include <string>
+#include "../lab11.4/lab11.4.cpp"
+using namespace std;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestWriteNewFile)
+        {
+            // Створюємо вхідний файл
+            ofstream inputFile("input.txt");
+            inputFile << "3 3\n"; // Розмір матриці: 3x3
+            inputFile << "1 2 3\n";
+            inputFile << "4 5 6\n";
+            inputFile << "7 8 9\n";
+            inputFile.close();
+
+            // Викликаємо функцію для обробки
+            string inputFileName = "input.txt";
+            string outputFileName = "output.txt";
+
+            bool result = writeNewFile(inputFileName, outputFileName);
+            Assert::IsTrue(result, L"Функція writeNewFile повинна була успішно завершитися.");
+
+            // Читаємо вихідний файл і перевіряємо його вміст
+            ifstream outputFile(outputFileName);
+            Assert::IsTrue(outputFile.is_open(), L"Не вдалося відкрити вихідний файл.");
+
+            int n, m;
+            outputFile >> n >> m;
+
+            Assert::AreEqual(3, n, L"Кількість рядків повинна дорівнювати 3.");
+            Assert::AreEqual(3, m, L"Кількість стовпців повинна дорівнювати 3.");
+
+            int expectedMatrix[3][4] = {
+                {1, 2, 3, 3}, // Максимум у рядку: 3
+                {4, 5, 6, 6}, // Максимум у рядку: 6
+                {7, 8, 9, 9}  // Максимум у рядку: 9
+            };
+
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m + 1; ++j) { // +1 для доданого максимуму
+                    int value;
+                    outputFile >> value;
+                    Assert::AreEqual(expectedMatrix[i][j], value, L"Невірне значення у вихідному файлі.");
+                }
+            }
+
+            outputFile.close();
+        }
+    };
+}
---
+++ lab11.4/lab11.4.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+void displayMenu();
+bool readMatrix(const string& filename, int& n, int& m, int**& matrix);
+int findMaxInRow(const int* row, int m);
+bool writeNewFile(const string& inputFile, const string& outputFile);
+void deleteMatrix(int** matrix, int n);
+
+int main() {
+    string inputFile, outputFile;
+    int choice;
+
+    while (true) {
+        displayMenu();
+        cin >> choice;
+
+        if (choice == 1) {
+            cout << "Введіть ім'я вхідного файлу: ";
+            cin >> inputFile;
+            cout << "Введіть ім'я вихідного файлу: ";
+            cin >> outputFile;
+
+            if (writeNewFile(inputFile, outputFile)) {
+                cout << "Файл успішно створено.\n";
+            }
+            else {
+                cout << "Помилка при створенні файлу.\n";
+            }
+        }
+        else if (choice == 0) {
+            break;
+        }
+        else {
+            cout << "Невірний вибір. Спробуйте ще раз.\n";
+        }
+    }
+    return 0;
+}
+
+void displayMenu() {
+    cout << "=== МЕНЮ ===\n";
+    cout << "1. Обробити файл\n";
+    cout << "0. Вийти\n";
+    cout << "Ваш вибір: ";
+}
+
+bool readMatrix(const string& filename, int& n, int& m, int**& matrix) {
+    ifstream inFile(filename);
+    if (!inFile) {
+        cerr << "Помилка: не вдалося відкрити файл " << filename << "\n";
+        return false;
+    }
+
+    inFile >> n >> m;
+    matrix = new int* [n];
+    for (int i = 0; i < n; ++i) {
+        matrix[i] = new int[m];
+        for (int j = 0; j < m; ++j) {
+            inFile >> matrix[i][j];
+        }
+    }
+    return true;
+}
+
+int findMaxInRow(const int* row, int m) {
+    int maxVal = numeric_limits<int>::min();
+    for (int j = 0; j < m; ++j) {
+        if (row[j] > maxVal) {
+            maxVal = row[j];
+        }
+    }
+    return maxVal;
+}
+
+bool writeNewFile(const string& inputFile, const string& outputFile) {
+    int n, m;
+    int** matrix = nullptr;
+
+    if (!readMatrix(inputFile, n, m, matrix)) {
+        return false;
+    }
+
+    ofstream outFile(outputFile);
+    if (!outFile) {
+        cerr << "Помилка: не вдалося створити файл " << outputFile << "\n";
+        deleteMatrix(matrix, n);
+        return false;
+    }
+
+    outFile << n << " " << m << "\n";
+    for (int i = 0; i < n; ++i) {
+        int maxVal = findMaxInRow(matrix[i], m);
+        for (int j = 0; j < m; ++j) {
+            outFile << matrix[i][j] << " ";
+        }
+        outFile << maxVal << "\n";
+    }
+
+    deleteMatrix(matrix, n);
+    return true;
+}
+
+void deleteMatrix(int** matrix, int n) {
+    for (int i = 0; i < n; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
